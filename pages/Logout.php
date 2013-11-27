0d627aa82d51ccbc05ebf957736e8c1a3aa4eae4
Fahv <robertstanday@gmail.com>
2013-11-26 21:18:08 -0800

    Really log out now

--- pages/Logout.php
+++ pages/Logout.php
@@ -2,16 +2,25 @@
 <head>
 <?php
 require_once('../src/admin.php');
-if(isset($_SESSION)){ 
-	if (ini_get("session.use_cookies")) {
+// Initialize the session.
+// If you are using session_name("something"), don't forget it now!
+session_start();
+
+// Unset all of the session variables.
+$_SESSION = array();
+
+// If it's desired to kill the session, also delete the session cookie.
+// Note: This will destroy the session, and not just the session data!
+if (ini_get("session.use_cookies")) {
     $params = session_get_cookie_params();
     setcookie(session_name(), '', time() - 42000,
         $params["path"], $params["domain"],
         $params["secure"], $params["httponly"]
     );
-	}
-	session_destroy();
-	}
+}
+
+// Finally, destroy the session.
+session_destroy();
 Admin::Redirect("login.php");
 ?>
 </head>
